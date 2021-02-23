135b796f9c18b0026b57c0b7ea90de94952af8c9
Thomas Butler <>
2021-02-23 16:57:17 -0500

    move 'go' test method to index

--- packages/@fndj/main/src/FnAudioBufferSourceNode.ts
+++ packages/@fndj/main/src/FnAudioBufferSourceNode.ts
@@ -233,27 +233,3 @@ export class FnAudioBufferSourceNode implements AudioBufferSourceNode, GainNode 
     this._constantNode.stop(when);
   }
 }
-
-export async function go(context = new AudioContext()) {
-  (window as any)['fnctx'] = context;
-
-  const response = await fetch("https://fuckingdj.blob.core.windows.net/test/Swedish%20House%20Mafia%20-%20Save%20The%20World%20(Zedd%20Remix).mp3");
-  const buffer = await response.arrayBuffer();
-  const audioBuffer = await context.decodeAudioData(buffer);
-
-
-  const source = await FnAudioBufferSourceNode.create(context, {
-    //buffer: audioBuffer,
-    baseTempo: 128,
-    tempo: 128
-  });
-  source.buffer = audioBuffer;
-  source.connect(context.destination);
-  // source.start();
-  // context.resume();
-  setTimeout(() => {
-    source.rate = 1.25;
-  }, 8000);
-  // source.tempo.setValueAtTime(140/128, context.currentTime + 10);
-  // source.playbackRate.setValueAtTime(128/140, context.currentTime + 10);
-}
