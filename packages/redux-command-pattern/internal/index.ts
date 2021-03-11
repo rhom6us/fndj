db9820ff864a425d9ff6bb4cdcbea43a708b9bd7
Thomas Butler <thomas@butler.software>
2021-03-11 16:35:43 -0500

    add new exports

--- packages/redux-command-pattern/internal/index.ts
+++ packages/redux-command-pattern/internal/index.ts
@@ -1,5 +1,5 @@
 export {getEventCreator} from './event-creator';
 export * from './command-creator';
 export {CommandFn, createCommandHandler} from './command-fn';
-export {createReducer} from './reducer-fn';
+export { createReducer, ReducerFn } from './reducer-fn';
 export {StandardEvent, StandardEventAny} from './standard-event';
