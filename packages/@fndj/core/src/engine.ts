172384478c17aff249a140960db58166f308d91d
Thomas Butler <thomas@butler.software>
2021-04-07 23:24:32 -0400

    a start to core

---
+++ packages/@fndj/core/src/engine.ts
@@ -0,0 +1,18 @@
+// import { Metronome, PlayControl, PlayerEngine, Transport } from 'waves-audio';
+// const transport = new Transport();
+// const controller = new PlayControl(transport);
+// const metrenome = new Metronome({ period: 1 / 128 });
+
+// declare const buffer: AudioBuffer;
+// const bufferPlayer = new PlayerEngine(buffer);
+
+// transport.add(metrenome);
+// transport.add(bufferPlayer);
+
+// controller.start();
+
+// metrenome.period = 1 / 130;
+
+export class Prep {
+
+}
