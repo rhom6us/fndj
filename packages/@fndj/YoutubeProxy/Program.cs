7ff985ad2643a1d1dfc9258a8eaa5d9d6e53c89d
rhom6us <rhom6us@users.noreply.github.com>
2022-07-18 10:29:17 -0400

    enable CORS

--- packages/@fndj/YoutubeProxy/Program.cs
+++ packages/@fndj/YoutubeProxy/Program.cs
@@ -19,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(p => p.WithMethods(HttpMethods.Get).AllowAnyOrigin().AllowAnyHeader());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
